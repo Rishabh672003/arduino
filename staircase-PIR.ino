c40084c942f7971d91913fefd78119d7d5343ccb
Rishabh <jharishabh2k3@gmail.com>
2023-02-02 09:06:20 +0530

    update

---
+++ staircase-PIR.ino
@@ -0,0 +1,27 @@
+int state = LOW;
+
+void setup(){
+  pinMode(2,INPUT);
+  pinMode(9,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop(){
+  int val = digitalRead(2);
+  if(val = HIGH){
+    digitalWrite(9,HIGH);
+    delay(500);
+    if(state = LOW){
+      serial.print("Motion detected");
+      state = HIGH;
+    }
+  }
+  else{
+    digitalWrite(9,LOW);
+    delay(500);
+    if(state = HIGH){
+      serial.print("Motion stopped");
+      state = LOW;
+    }
+  }
+}
